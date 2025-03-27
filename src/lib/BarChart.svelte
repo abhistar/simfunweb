82be449525b15d348402e4e937b91f3a5d9e7bdb
Abhinav Jain <jainabhinav2412@gmail.com>
2025-03-28 02:16:17 +0530

    Add simulation and chart

---
+++ src/lib/BarChart.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { BarChart, type BarChartData, type BarChartOptions } from "chartist";
+  import "chartist/dist/index.css"; // Import Chartist styles
+
+  let type: string = "Line"; // Accepts "Line", "Bar", "Pie"
+
+  let { data, height } = $props();
+
+  let options: BarChartOptions = {
+    high: height,
+    low: 0,
+    axisX: {
+      labelInterpolationFnc(value, index) {
+        return index % 2 === 0 ? value : null;
+      },
+    },
+  };
+
+  let chartContainer: Element;
+
+  onMount(() => {
+    if (chartContainer) {
+      new BarChart(chartContainer, data, options);
+    }
+  });
+
+  $effect(() => {
+    if (chartContainer) {
+      options.high = height;
+      new BarChart(chartContainer, data, options);
+    }
+  });
+</script>
+
+<div bind:this={chartContainer} class="ct-chart"></div>
---
+++ src/lib/Simulator.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import type { BarChartData } from "chartist";
+  import BarChart from "./BarChart.svelte";
+
+  let initialPace: number = $state(1.84);
+  let paceDelta: number = $state(0.01);
+  let deltaProbability: number = $state(0.33);
+  let targetTimeMinutes: number = $state(30);
+  let modelRangeInPercent: number = $state(10);
+  let numberOfSimulations: number = $state(10);
+  let bucketSizeInSecond: number = $state(30);
+
+  let stepTimeCounterData: BarChartData | null = $state(null);
+
+  const stepsTimeCount = () => {
+    let targetTimeSeconds = targetTimeMinutes * 60;
+    let numberOfSteps = initialPace * targetTimeSeconds;
+
+    let minTargetTime = (targetTimeSeconds * (100 - modelRangeInPercent)) / 100;
+    let maxTargetTime = (targetTimeSeconds * (100 + modelRangeInPercent)) / 100;
+
+    let stepTimeCounts = new Map<number, number>();
+
+    for (let t = minTargetTime; t <= maxTargetTime; t += bucketSizeInSecond) {
+      stepTimeCounts.set(t / 60, 0);
+    }
+
+    for (let i = 0; i < numberOfSimulations; i++) {
+      let timeTaken = getTimeTakenForSteps(numberOfSteps);
+
+      let timeBucket: number | null = null;
+      for (let t = minTargetTime; t <= maxTargetTime; t += bucketSizeInSecond) {
+        if (timeTaken >= t && timeTaken < t + bucketSizeInSecond) {
+          timeBucket = t / 60;
+          break;
+        }
+      }
+      if (timeBucket != null) {
+        let timeCount = stepTimeCounts.get(timeBucket) ?? 0;
+        stepTimeCounts.set(timeBucket, timeCount + 1);
+      }
+    }
+    let labels = [];
+    let series = [];
+    for (const entry of stepTimeCounts.entries()) {
+      const [key, value] = entry;
+      labels.push(key.toFixed(2));
+      series.push(value);
+    }
+    stepTimeCounterData = {
+      labels: labels,
+      series: [series],
+    };
+  };
+
+  const getTimeTakenForSteps = (stepsLimit: number): number => {
+    let pace = initialPace;
+    let steps = 0;
+    let totalTime = 0;
+    while (steps + pace * 60 <= stepsLimit) {
+      steps += pace * 60;
+      totalTime += 60;
+      let randomNumber = Math.random();
+      if (randomNumber <= deltaProbability) {
+        pace -= paceDelta;
+      } else if (randomNumber > 1 - deltaProbability) {
+        pace += paceDelta;
+      }
+    }
+
+    if (steps < stepsLimit) {
+      totalTime += (stepsLimit - steps) / pace;
+    }
+    return totalTime;
+  };
+</script>
+
+<div class="simContainer">
+  <div>Initial Pace per second</div>
+  <input type="number" bind:value={initialPace} min="1" max="3" />
+  <div>Change delta for pace per second</div>
+  <input type="number" bind:value={paceDelta} min="0" max="0.03" />
+  <div>Probability of change</div>
+  <input type="number" bind:value={deltaProbability} min="0" max="1" />
+  <div>Target time in minutes</div>
+  <input type="number" bind:value={targetTimeMinutes} />
+  <div>Challenge range in percent</div>
+  <input type="number" bind:value={modelRangeInPercent} min="0" max="100" />
+  <div>Bucket size of bars in seconds</div>
+  <input type="number" bind:value={bucketSizeInSecond} min="0" />
+  <div>Number of simulations to run</div>
+  <input type="number" bind:value={numberOfSimulations} min="1" max="100000" />
+
+  <button onclick={stepsTimeCount}> Click to get simfun </button>
+  {#if stepTimeCounterData != null}
+    <BarChart data={stepTimeCounterData} height={numberOfSimulations} />
+  {/if}
+</div>
