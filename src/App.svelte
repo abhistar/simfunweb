cd1f6d58b6b6ab0adb7747187e52d263dc6e9d65
Abhinav Jain <jainabhinav2412@gmail.com>
2025-03-29 02:11:13 +0530

    Fix styling

--- src/App.svelte
+++ src/App.svelte
@@ -6,21 +6,26 @@
 </script>
 
 <main>
-  <div class="h-full justify-center">
-    <div class="bg-teal-600 h-64 w-full">
-      <div class="absolute top-8 justify-self-anchor-center">
-        <div class="text-white text-center text-6xl font-semibold italic">
-          SimFun!
-        </div>
-        <div class="text-white text-center text-base">
-          Simple Simluations For Fun
+  <div class="flex flex-col justify-between">
+    <div>
+      <div class="bg-teal-600 h-64 w-full">
+        <div class="relative top-8 justify-self-anchor-center">
+          <div class="text-white text-center text-6xl font-semibold italic">
+            SimFun!
+          </div>
+          <div class="text-white text-center text-base">
+            Simple Simluations For Fun
+          </div>
         </div>
       </div>
+      <div
+        class="w-3/4 border-2 rounded-lg shadow-lg relative -top-24 justify-center justify-self-anchor-center opacity-100 bg-white z-10"
+      >
+        <Simulator />
+      </div>
     </div>
-    <div
-      class="w-3/4 border-2 rounded-lg shadow-lg justify-center absolute top-32 justify-self-anchor-center opacity-100 bg-white"
-    >
-      <Simulator />
+    <div class="h-32 bg-teal-600 flex items-center justify-center text-white">
+      <div>Made with love: abhistar</div>
     </div>
   </div>
 </main>
--- src/lib/Simulator.svelte
+++ src/lib/Simulator.svelte
@@ -4,7 +4,7 @@
   import type { BarChartData } from "chartist";
   import BarChart from "./BarChart.svelte";
 
-  let initialPace: number = $state(1.84);
+  let initialStepRate: number = $state(1.84);
   let paceDelta: number = $state(0.01);
   let deltaProbability: number = $state(0.33);
   let targetTimeMinutes: number = $state(30);
@@ -17,7 +17,7 @@
 
   const stepsTimeCount = () => {
     let targetTimeSeconds = targetTimeMinutes * 60;
-    let numberOfSteps = initialPace * targetTimeSeconds;
+    let numberOfSteps = initialStepRate * targetTimeSeconds;
 
     let minTargetTime =
       (targetTimeSeconds * (100 - 2 * modelRangeInPercent)) / 100;
@@ -59,7 +59,7 @@
   };
 
   const getTimeTakenForSteps = (stepsLimit: number): number => {
-    let pace = initialPace;
+    let pace = initialStepRate;
     let steps = 0;
     let totalTime = 0;
     while (steps + pace * averageTimeInSecondForPaceChange <= stepsLimit) {
@@ -81,15 +81,71 @@
 </script>
 
 <div class="flex flex-col w-full gap-4 p-8">
-  <div>Adam walk simulator</div>
-  <div>jhjhkjk</div>
+  <div class="w-full p-6">
+    <div class="text-lg font-semibold">Adam walk simulator</div>
+    <div class="text-sm text-justify">
+      In season 10 of <a
+        href="https://youtu.be/v4KHDMvFQgg?t=1841"
+        class="text-blue-800 underline"
+        target="_blank">Jet Lag: The Game</a
+      >
+      where two team play a game to control Australia's 8 regions, Adam Chase, member
+      of team red takes up on the challenge of sensing the passage of 30 mins with
+      the use of a makeshift clock. In order to achieve the given time, Adam measures
+      his walking pace and begins to count for exactly 3312 steps which would amount
+      to 30 minutes per his walking pace of 1.84 steps/second. Inspired by the game
+      show and by another video on Markov chains by
+      <a
+        href="https://www.youtube.com/@cadaeicstudios"
+        class="text-blue-800 underline"
+        target="_blank">Cadaeic Studios</a
+      >
+      on the same challenge, here's the simulation which plots the number of times
+      the walk could have ended at a given time, given the target time Adam wanted
+      to measure by his makeshift 11-o-clock.<br /> <br />Input parameters are
+      as follows:
+      <ul class="list-disc">
+        <li>
+          <b>Initial Steps per second rate:</b> Defines the number of steps per second
+          as measured before starting the challenge.
+        </li>
+        <li>
+          <b>Change delta for steps per second:</b> The change in step rate per second
+          after every couple of seconds. This is the amount by which the step rate
+          can increase or decrease every couple of steps.
+        </li>
+        <li>
+          <b>Probability of change:</b>The probability that the step rate will
+          increase or decrease. For probability p the chance of increase is p,
+          chance of decrease is p and chance of same step rate is 1-2*p.
+        </li>
+        <li>
+          <b>Target time in minutes:</b> Time which is supposed to be passed and
+          measured by our makeshift clock.
+        </li>
+        <li>
+          <b>Challenge range in percent:</b> If the makeshift clock time is called
+          within this range of target time then the challenge is successfully completed.
+        </li>
+        <li>
+          <b>Bucket size of bars in seconds:</b> Determines the time range in seconds
+          within which all times measured will be counted into.
+        </li>
+        <li>
+          <b>Average time in seconds for change in pace:</b> The average time in
+          which the step rate can change.
+        </li>
+        <li><b>Number of simulations to run:</b> Self explanatory.</li>
+      </ul>
+    </div>
+  </div>
   <div class="w-full grid grid-cols-2 gap-4">
     <div class="min-w-fit">
-      <div>Initial Pace per second</div>
-      <Input type="number" bind:value={initialPace} min="1" max="3" />
+      <div>Initial steps per second rate</div>
+      <Input type="number" bind:value={initialStepRate} min="1" max="3" />
     </div>
     <div class="min-w-fit">
-      <div>Change delta for pace per second</div>
+      <div>Change delta for steps per second</div>
       <Input type="number" bind:value={paceDelta} min="0" max="0.03" />
     </div>
     <div class="min-w-fit">
