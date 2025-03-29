3e990f5f471dbe4981d17b46d1baf6d2341ed57b
Abhinav Jain <jainabhinav2412@gmail.com>
2025-03-29 14:46:10 +0530

    Add new logo

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   import "./app.css";
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
   import Simulator from "./lib/Simulator.svelte";
 </script>
 
 <main>
   <div class="flex flex-col justify-between">
     <div>
-      <div class="bg-teal-600 h-64 w-full">
+      <div class="bg-emerald-600 h-64 w-full">
         <div class="relative top-8 justify-self-anchor-center">
           <div class="text-white text-center text-6xl font-semibold italic">
             SimFun!
@@ -24,7 +22,9 @@
         <Simulator />
       </div>
     </div>
-    <div class="h-32 bg-teal-600 flex items-center justify-center text-white">
+    <div
+      class="h-32 bg-emerald-600 flex items-center justify-center text-white"
+    >
       <div>Made with love: abhistar</div>
     </div>
   </div>
